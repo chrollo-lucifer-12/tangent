8e83b239bcd8fdac8982c544385815a6e024744e
sahil-singh1107 <sahilpython@gmail.com>
2025-02-21 09:51:36 +0530

    complete

--- src/hooks/use-socket.tsx
+++ src/hooks/use-socket.tsx
@@ -7,7 +7,7 @@ const useSocket = () => {
     const actions = useBearActions();
     const [isConnected, setIsConnected] = useState<boolean>(false);
     useEffect(() => {
-        const newSocket = new WebSocket("https://tangent-server.onrender.com");
+        const newSocket = new WebSocket(process.env.NEXT_PUBLIC_SOCKET!);
 
         newSocket.onopen = () => {
             setIsConnected(true);
